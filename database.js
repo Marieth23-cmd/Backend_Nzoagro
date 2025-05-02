14f86e472b16ddb27fef7c567054bd3718c7d10d
Marieth270 <134919554+Marieth270@users.noreply.github.com>
2025-05-02 23:31:31 +0100

    Ajuste na configuração de conexão com o banco no Railway

--- database.js
+++ database.js
@@ -1,16 +1,17 @@
- const mysql= require("mysql2");
+require('dotenv').config();
+const mysql= require("mysql2");
   
- const pool=mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"nzoagro1",
-    waitForConnections:true,
-    connectionLimit:10,
-    queueLimit:0,
-    
-  });
-  
+ const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT    
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
 
 
   pool.getConnection((err,connection)=>{
--- seedAdmin.js
+++ seedAdmin.js
@@ -1,24 +1,24 @@
-require('dotenv').config()
-const conexao=require('./database')
-const bcrypt= require('bcryptjs')
+// require('dotenv').config()
+// const conexao=require('./database')
+// const bcrypt= require('bcryptjs')
 
- async function seedAdmin(){
-    const email= process.env.ADMIN_EMAIL
-    const password=process.env.ADMIN_PASSWORD
-    const hashPassword= await bcrypt.hash(password , 10)
+//  async function seedAdmin(){
+//     const email= process.env.ADMIN_EMAIL
+//     const password=process.env.ADMIN_PASSWORD
+//     const hashPassword= await bcrypt.hash(password , 10)
 
-    const[rows]= await conexao.promise().query('Select * from usuarios WHERE email=?' , [email])
+//     const[rows]= await conexao.promise().query('Select * from usuarios WHERE email=?' , [email])
     
     
-    if(rows.length>0){
-        console.log('Email já cadastrado' , rows)
-        return
-      }
-      await conexao.promise().query(' Insert into usuarios( nome , email, senha , tipo_usuario) values(?,?,?,?)'
-        ,['Administrador' , email,hashPassword,'Admin' ]
-      )
-      console.log('Administrador cadaastrado com sucesso')
+//     if(rows.length>0){
+//         console.log('Email já cadastrado' , rows)
+//         return
+//       }
+//       await conexao.promise().query(' Insert into usuarios( nome , email, senha , tipo_usuario) values(?,?,?,?)'
+//         ,['Administrador' , email,hashPassword,'Admin' ]
+//       )
+//       console.log('Administrador cadaastrado com sucesso')
 
 
-}
-seedAdmin().catch(err=> console.log(err))
+// }
+// seedAdmin().catch(err=> console.log(err))
